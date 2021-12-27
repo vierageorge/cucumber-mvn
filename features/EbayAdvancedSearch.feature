af82b17b4e98948447150d45a6e5d4207d84c89f
Jorge Viera <jorge.viera@morningstar.com>
2021-12-27 11:22:27 -0500

    Adding a Test Runner

---
+++ features/EbayAdvancedSearch.feature
@@ -0,0 +1,6 @@
+Feature: Ebay Advanced Search Page Scenarios
+
+  Scenario: Ebay logo on Advanced Search
+    Given I am on Ebay Advanced Search
+    When I click on Ebay Logo
+    Then I am navigated to Ebay Home Page
---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,14 @@
+package testRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"features"},
+        glue = {"steps"},
+        plugin = {"pretty", "html:reports/Report1.html"}
+)
+public class TestRunner {
+}
